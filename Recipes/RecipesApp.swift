7fe6a2352be3bb9f40c0916394ba7a5f1b456605
Victor Ulloa <alejandroekas@gmail.com>
2024-11-14 00:28:32 -0500

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Victor Ulloa on 2024-11-14.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
